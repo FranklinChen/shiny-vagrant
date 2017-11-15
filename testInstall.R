a08f13bec609476fc3364233d8d15151f3bbc3b1
franklinr <franklinr@users.noreply.github.com>
2017-11-15 15:20:31 +0000

    this file tests and install R packages

---
+++ testInstall.R
@@ -0,0 +1,28 @@
+
+testinstall <- function(somepackage,repos='https://cran.ma.imperial.ac.uk/'){
+  print(paste("testing ",somepackage))
+  if(!require(somepackage,character.only = TRUE)){
+    install.packages(somepackage)
+  }
+}
+testinstall('shiny', repos='https://cran.rstudio.com/')
+testinstall('rmarkdown', repos='https://cran.rstudio.com/')
+testinstall('DT', repos='https://cran.rstudio.com/')
+devtools::install_github('rstudio/DT@feature/editor')
+testinstall('devtools')
+testinstall('shinycssloaders')
+testinstall('ngram')
+#testinstall('xml2')
+#testinstall('dplyr')
+#testinstall('doParallel')
+
+# R -e "install.packages('shiny', repos='https://cran.rstudio.com/')"
+# #R -e "install.packages('RCurl',repos='https://cran.ma.imperial.ac.uk/')" 
+# R -e "install.packages('rmarkdown', repos='http://cran.rstudio.com/')"
+# R -e "install.packages('DT',repos='https://cran.rstudio.com/')"
+# R -e "install.packages('devtools',repos='https://cran.ma.imperial.ac.uk/')" 
+# R -e "install.packages('shinycssloaders',repos='https://cran.ma.imperial.ac.uk/')"
+# R -e "install.packages('ngram',repos='https://cran.ma.imperial.ac.uk/')" 
+# R -e "install.packages('xml2',repos='https://cran.ma.imperial.ac.uk/')" 
+# R -e "install.packages('dplyr',repos='https://cran.ma.imperial.ac.uk/')" 
+# R -e "install.packages('doParallel',repos='https://cran.ma.imperial.ac.uk/')" 

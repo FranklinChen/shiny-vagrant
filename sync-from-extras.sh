830c705e7bd5f821048aa5c2a05d30d24677683b
franklinr <franklinr@users.noreply.github.com>
2017-11-18 16:59:42 +0000

    removed folders

--- sync-from-extras.sh
+++ sync-from-extras.sh
@@ -4,7 +4,6 @@ extras=$HOME/shiny-vagrant-extras
 
 for i in csvcorpora ngrams actualcsv
 do
-	rm -f ./storage/$i
 	ln -s $extras/$i/ ./storage/$i
 #	rsync -avz --delete $extras/$i/ ./storage/$i
 done

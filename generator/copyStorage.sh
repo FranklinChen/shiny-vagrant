705ab7ae71b2937e9a510008b605831b0ff02547
franklinr <franklinr@users.noreply.github.com>
2017-12-03 15:29:30 +0000

    Create copyStorage.sh

---
+++ generator/copyStorage.sh
@@ -0,0 +1,5 @@
+mv workfiles/*.rds ../storage/
+mv workfiles/*.csv ../storage/
+mv workfiles/*.txt ../storage/
+mv workfiles/actualcsv/*.csv ../storage/actualcsv/
+mv workfiles/ngrams/*.rds ../storage/ngrams

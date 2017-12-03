2c1b6024b179ee6e42c892b182d554cffd94e6f8
franklinr <franklinr@users.noreply.github.com>
2017-12-03 23:46:19 +0000

    Update downloadchildes.sh

--- generator/downloadchildes.sh
+++ generator/downloadchildes.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+cd /Users/macw/shiny-vagrant/generator/
 rm -rf workfiles
 /bin/bash getCorp.sh
 /bin/bash makeCorp.sh

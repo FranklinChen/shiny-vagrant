0a2f19a56f5c2cab7de2709b55f35434189f8715
franklinr <franklinr@users.noreply.github.com>
2017-12-03 23:49:17 +0000

    Update copyStorage.sh

--- generator/copyStorage.sh
+++ generator/copyStorage.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 mv workfiles/*.rds ../storage/
 mv workfiles/*.csv ../storage/
 mv workfiles/*.txt ../storage/
 mv workfiles/actualcsv/*.csv ../storage/actualcsv/
-mv workfiles/ngrams/*.rds ../storage/ngrams
+mv workfiles/ngramsdir/*.rds ../storage/ngrams

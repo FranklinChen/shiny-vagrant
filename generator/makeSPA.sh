69b6be0d084f0202f4ed50a00c228a799572e764
franklinr <franklinr@users.noreply.github.com>
2017-12-03 23:47:50 +0000

    Update makeSPA.sh

--- generator/makeSPA.sh
+++ generator/makeSPA.sh
@@ -1,7 +1,7 @@
+#!/bin/sh
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 cd workfiles
 mkdir actualcsv
 nice -n 19 Rscript ../convertRDS2CSV.R > out2
 rm -f bigspa.csv 
 nice -n 19 python3 ../createbigspa.py > out3
- 
-

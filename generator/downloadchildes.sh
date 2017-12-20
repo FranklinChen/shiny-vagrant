eee05f16b172cf948bd0e7fb7023606fc827abac
franklinr <franklinr@users.noreply.github.com>
2017-12-20 20:49:13 +0000

    Update downloadchildes.sh

--- generator/downloadchildes.sh
+++ generator/downloadchildes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
-cd /Users/macw/shiny-vagrant/generator/
+cd /Users/franklin/shiny-vagrant/generator/
 rm -rf workfiles
 /bin/bash getCorp.sh
 /bin/ls -lha workfiles | awk '{print $6,$7,$8,$5,$9}' | mail -s childesgen chang.franklin@gmail.com 

d9c1003235b1333cf2275af27a3a9e2eeef2dce1
franklinr <franklinr@users.noreply.github.com>
2017-12-23 20:27:43 +0000

    Add files via upload

---
+++ generator/syntax.R
@@ -0,0 +1,31 @@
+require(dplyr)
+library(jsonlite)
+
+fl = list.files("csvfolderMake","^.+.rds",full.names=T)
+print(fl)
+
+nfl = str_replace(fl,"csvfolderMake","googletags")
+#nfl = str_replace(nfl,"rds","rds")
+
+for (i in 1:length(fl)){
+  if (!file.exists(nfl[i])){
+    print(fl[i])
+    df = readRDS(fl[i])
+    
+    all = data.frame()
+    for (i in 1:length(df$w)){
+      var = as.character(df$w[2])
+      cmd = paste("/Users/chang/Downloads/google-cloud-sdk/bin/gcloud ml language analyze-syntax --content=\"",var,"\"",sep="")
+      out = system(cmd, intern = TRUE)
+      js = fromJSON(paste(out,collapse=" "))
+      jsdf = flatten(js$tokens)
+      jsdf$sent = js$sentences$text$content
+      jsdf$sentnum = i
+      all = bind_rows(all,jsdf)
+    }
+    print(head(all))
+    saveRDS(all,nfl[i])
+  }
+}
+
+

27814fcd5926e9b56e57d00766f1a23d8744f12d
franklinr <franklinr@users.noreply.github.com>
2017-12-25 08:47:39 +0000

    Update makeGoogle.sh

--- generator/makeGoogle.sh
+++ generator/makeGoogle.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Applications/VMware Fusion.app/Contents/Public
 cd workfiles
-nohup /usr/bin/Rscript ../syntax.R > out5 
+nohup Rscript ../syntax.R > out5 

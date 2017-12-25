22b62d572c74373bb8fe9d3f07387a1ea73d6284
franklinr <franklinr@users.noreply.github.com>
2017-12-25 08:45:59 +0000

    Update makeCorp.sh

--- generator/makeCorp.sh
+++ generator/makeCorp.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Applications/VMware Fusion.app/Contents/Public
 cd workfiles
 nohup nice -n 19 Rscript ../createCorpora18.R 0 > out 

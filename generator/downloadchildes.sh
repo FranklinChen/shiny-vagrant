e315df81b12274a0190472715b6ef8b95f14fa39
franklinr <franklinr@users.noreply.github.com>
2017-12-25 08:45:30 +0000

    Update downloadchildes.sh

--- generator/downloadchildes.sh
+++ generator/downloadchildes.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Applications/VMware Fusion.app/Contents/Public
 cd /Users/franklin/shiny-vagrant/generator/
 rm -rf workfiles
 /bin/bash getCorp.sh

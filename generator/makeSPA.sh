5df4685eb688370764a94359460ea61ac8bfd53a
franklinr <franklinr@users.noreply.github.com>
2017-12-25 09:12:01 +0000

    Update makeSPA.sh

--- generator/makeSPA.sh
+++ generator/makeSPA.sh
@@ -3,7 +3,6 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 cd workfiles
-mkdir actualcsv
 nohup Rscript ../convertRDS2CSV.R > out2
 rm -f bigspa.csv 
 nohup python3 ../createbigspa.py > out3

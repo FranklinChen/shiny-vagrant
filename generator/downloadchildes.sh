6fdaa6508c9cf2584608b27511afbe219e44c51e
franklinr <franklinr@users.noreply.github.com>
2017-12-27 06:14:51 +0000

    Update downloadchildes.sh

--- generator/downloadchildes.sh
+++ generator/downloadchildes.sh
@@ -2,13 +2,13 @@
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
-cd /Users/franklin/shiny-vagrant/generator/
+cd ~/shiny-vagrant/generator/
 rm -rf workfiles
 /bin/bash getCorp.sh
-/bin/ls -lhad workfiles/data-xml | awk '{print $6,$7,$8,$5,$9}' > /Users/franklin/macwshiny/storage/timestamp.txt
+/bin/ls -lhad workfiles/data-xml | awk '{print $6,$7,$8,$5,$9}' > storage/timestamp.txt
 /bin/bash makeCorp.sh
-/bin/ls -lhad workfiles/csvfolderMake | awk '{print $6,$7,$8,$5,$9}' > /Users/franklin/macwshiny/storage/timestamp.txt
+/bin/ls -lhad workfiles/csvfolderMake | awk '{print $6,$7,$8,$5,$9}' > storage/timestamp.txt
 /bin/bash makeSPA.sh
-/bin/ls -lha workfiles/bigspa.csv | awk '{print $6,$7,$8,$5}' > /Users/franklin/macwshiny/storage/timestamp.txt
+/bin/ls -lha workfiles/bigspa.csv | awk '{print $6,$7,$8,$5}' > storage/timestamp.txt
 /bin/bash copyStorage.sh
 /bin/ls -lha workfiles | awk '{print $6,$7,$8,$5,$9}' | mail -s childesgen chang.franklin@gmail.com 

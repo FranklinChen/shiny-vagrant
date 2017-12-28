2c7ed31c737a749555809539176fb580f4331f58
franklinr <franklinr@users.noreply.github.com>
2017-12-28 14:27:10 +0000

    Update reloadprovision.sh

--- reloadprovision.sh
+++ reloadprovision.sh
@@ -3,5 +3,9 @@ PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 echo "${0%/*}"
 cd "${0%/*}"
 git pull
-vagrant reload
-vagrant provision
+if [ $1 = "skip" ]; then
+  echo "skip reload provision"
+else
+  vagrant reload
+  vagrant provision
+fi

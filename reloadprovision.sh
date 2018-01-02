2ff180cb5d04a7a190dbe27ad4c3e26f0b2ebf39
franklinr <franklinr@users.noreply.github.com>
2018-01-02 20:56:38 +0000

    Update reloadprovision.sh

--- reloadprovision.sh
+++ reloadprovision.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
-
+export USER=macw
 echo "${0%/*}"
 cd "${0%/*}"
+
 git pull
+
 if [[ $1 = "skip" ]]; then
   echo "skip reload provision"
 else

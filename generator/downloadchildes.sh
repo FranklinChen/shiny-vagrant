8851322463bb3c0106647710e3f7523869c35b6c
franklinr <franklinr@users.noreply.github.com>
2018-01-11 17:33:34 +0000

    Update downloadchildes.sh

--- generator/downloadchildes.sh
+++ generator/downloadchildes.sh
@@ -6,10 +6,7 @@ echo "${0%/*}"
 cd "${0%/*}"
 rm -rf workfiles
 /bin/bash getCorp.sh
-/bin/ls -lhad workfiles/data-xml | awk '{print $6,$7,$8,$5,$9}' > storage/timestamp.txt
 /bin/bash makeCorp.sh
-/bin/ls -lhad workfiles/csvfolderMake | awk '{print $6,$7,$8,$5,$9}' > storage/timestamp.txt
 /bin/bash makeSPA.sh
-/bin/ls -lha workfiles/bigspa.csv | awk '{print $6,$7,$8,$5}' > storage/timestamp.txt
 /bin/bash copyStorage.sh
 /bin/ls -lha workfiles | awk '{print $6,$7,$8,$5,$9}' | mail -s childesgen chang.franklin@gmail.com 

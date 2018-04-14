40499c8229e54759cab8ec18bec1578129d4ae86
franklinr <franklinr@users.noreply.github.com>
2018-04-14 09:12:14 +0900

    Update makeCorp.sh

--- generator/makeCorp.sh
+++ generator/makeCorp.sh
@@ -5,7 +5,9 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 cd workfiles
 nohup nice -n 19 Rscript ../createCorpora18.R 1 > out 
 nohup nice -n 19 Rscript ../createCorpora18.R 1 >> out 
-nohup nice -n 19 Rscript ../createCorpora18.R 0 > out1 
+nohup nice -n 19 Rscript ../createCorpora18.R 2 > out1 
+nohup nice -n 19 Rscript ../createCorpora18.R 2 >> out1
+nohup nice -n 19 Rscript ../createCorpora18.R 0 >> out1 
 #nohup nice -n 19 Rscript ../createCorpora18.R 2 > out1 
 #nohup nice -n 19 Rscript ../createCorpora18.R 3 >> out1 
 #nohup nice -n 19 Rscript ../createCorpora18.R 4 >> out1 

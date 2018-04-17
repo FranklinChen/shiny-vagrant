6d40888380fc82be738e61e005e52aac84cc3679
franklinr <franklinr@users.noreply.github.com>
2018-04-17 17:18:45 +0900

    Update getCorp.sh

--- generator/getCorp.sh
+++ generator/getCorp.sh
@@ -2,8 +2,9 @@
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 mkdir workfiles
 cd workfiles
-rm -rf data-xml
-wget -c -e robots=off -r -l inf --no-remove-listing -nH --no-parent -R 'index.html*'  http://childes.talkbank.org/data-xml/
-find data-xml -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`" "$filename"; done;
-find data-xml -name "*.zip" | xargs rm 
-/bin/ls -lhad data-xml | awk '{print $6,$7,$8,$5,$9}' > ../storage/timestamp.txt
+if [ ! -d "data-xml" ]; then
+  wget -c -e robots=off -r -l inf --no-remove-listing -nH --no-parent -R 'index.html*'  http://childes.talkbank.org/data-xml/
+  find data-xml -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`" "$filename"; done;
+  find data-xml -name "*.zip" | xargs rm 
+  /bin/ls -lhad data-xml | awk '{print $6,$7,$8,$5,$9}' > ../storage/timestamp.txt
+fi

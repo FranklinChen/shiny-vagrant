2343e341f36774c12ac32affcf28f957e3adcd23
franklinr <franklinr@users.noreply.github.com>
2018-06-24 06:25:15 +0900

    Update downloadchildes.sh

--- generator/downloadchildes.sh
+++ generator/downloadchildes.sh
@@ -5,9 +5,9 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 echo "${0%/*}"
 cd "${0%/*}"
 rm -rf workfiles
-/bin/bash getCorp.sh
-/bin/bash makeCorp.sh
-/bin/bash makeSPA.sh
-/bin/bash copyStorage.sh
+/bin/bash getCorp.sh > hist
+/bin/bash makeCorp.sh >> hist
+/bin/bash makeSPA.sh >> hist
+/bin/bash copyStorage.sh >> hist
 /bin/ls -lha workfiles | awk '{print $6,$7,$8,$5,$9}' | mail -s childesgen chang.franklin@gmail.com 
-rm -rf workfiles
+#rm -rf workfiles

3ab48e5418e1ef4ad3670617f4f4b8b319feb2f7
franklinr <franklinr@users.noreply.github.com>
2019-09-06 17:21:56 -0700

    Update makeCorp.sh

--- generator/makeCorp.sh
+++ generator/makeCorp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/R.framework/Resources/bin
 cd workfiles
 nice -n 19 Rscript ../createCorpora18.R 1 > out 
 nice -n 19 Rscript ../createCorpora18.R 2 > out1 
